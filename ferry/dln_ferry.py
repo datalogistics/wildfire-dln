569be542c9dbcbc0aa360a1a2a6cebe4ef663d27
Ezra Kissel <ezkissel@indiana.edu>
2017-11-01 13:08:44 -0400

    Initial commit with some skeleton code.

---
+++ ferry/dln_ferry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import libdlt
+
+def main():
+    parser = argparse.ArgumentParser(description="DLN Mobile Ferry Agent")
+    print("Hello World, i'm a little ferry")
+
+if __name__ == "__main__":
+    main()
---
+++ ferry/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import sys
+from setuptools import setup
+
+version = "0.1"
+
+sys.path.append(".")
+if sys.version_info[0] < 3: 
+    print("------------------------------")
+    print("Must use python 3.0 or greater", file=sys.stderr)
+    print("Found python version ", sys.version_info, file=sys.stderr)
+    print("Installation aborted", file=sys.stderr)
+    print("------------------------------")
+    sys.exit()
+
+setup(
+    name = "dln-ferry",
+    version = version,
+    author = "Ezra Kissel",
+    author_email="ezkissel@indiana.edu",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    install_requires=[
+        "setuptools",
+        "lace",
+        "unisrt",
+        "libdlt"
+    ],
+    dependency_links=[
+        "git+https://github.com/periscope-ps/lace.git/@master#egg=lace",
+        "git+https://github.com/periscope-ps/unisrt.git/@develop#egg=unisrt",
+        "git+https://github.com/datalogistics/libdlt.git/@develop#egg=libdlt",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'dln_ferry = dln_ferry:main'
+        ]
+    },
+)
