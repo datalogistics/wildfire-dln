51e21ccec57763337c137cc8dc27d0cbe74c1f11
Jeremy Musser <jemusser@indiana.edu>
2017-12-04 22:28:09 -0500

    Added IDMS service

--- idms/idms/handlers/policy.py
+++ idms/idms/handlers/policy.py
@@ -13,8 +13,12 @@ class PolicyHandler(_BaseHandler):
     @falcon.after(_BaseHandler.encode_response)
     @get_body
     def on_post(self, req, resp, body):
-        print(body)
-        
+        assert("files" in body and "policy" in body, "Bad request form client")
+        if 'ferry_name' not in body['policy']:
+            raise ValueError("Invalid format in policy - {}".format(body['policy']))
+        policy = self._db._policies[body['policy']]
+        self.rt.pending_exnodes[policy['ferry_name']].extend(list(map(lambda x: (self.rt.find(x), policy['data_lifetime']]), body['files'])))
+    
     #@falcon.before(_BaseHandler.do_auth)
     @get_body
     def on_get(self, req, resp, body):
