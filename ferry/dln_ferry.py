10f6107c221dc11c4f80732d81aaaa887d2d4a00
Ezra Kissel <ezkissel@indiana.edu>
2017-12-04 21:39:27 -0500

    The ferry now downloads files.

--- ferry/dln_ferry.py
+++ ferry/dln_ferry.py
@@ -7,20 +7,24 @@ import socket
 import threading
 import lace, logging
 
-from libdlt.util import util
-from libdlt.depot import Depot
+import libdlt
 from unis.models import Exnode, Service, Node, schemaLoader
 from unis.runtime import Runtime
 
 GEOLOC="http://unis.crest.iu.edu/schema/ext/dln/1/geoloc#"
 FERRY_SERVICE="http://unis.crest.iu.edu/schema/ext/dln/1/ferry#"
+
 UNIS_URL="http://localhost:8888"
+LOCAL_UNIS_HOST="localhost"
+LOCAL_UNIS_PORT=9000
+DOWNLOAD_DIR="/depot/web"
+
 log = None
 
 DLNFerry = schemaLoader.get_class(FERRY_SERVICE)
 GeoLoc = schemaLoader.get_class(GEOLOC)
 
-def register(rt, name):
+def register(rt, name, fqdn):
     n = rt.nodes.where({"name": name})
     try:
         n = next(n)
@@ -37,7 +41,8 @@ def register(rt, name):
         s.runningOn = n
         s.serviceType="datalogistics:wdln:ferry"
         s.name = name
-        s.accessPoint = "ibp://{}:6714".format(name)
+        s.accessPoint = "ibp://{}:6714".format(fqdn)
+        s.unis_url = "http://{}:{}".format(fqdn, LOCAL_UNIS_PORT)
         s.status = "READY"
         rt.insert(s, commit=True)
 
@@ -64,20 +69,37 @@ def register(rt, name):
 def init_runtime(url):
     while True:
         try:
-            rt = Runtime([{"default": True, "url": url}],
+            rt = Runtime([{"default": True, "url": url},
+                          {"url": "http://{}:{}".format(LOCAL_UNIS_HOST,LOCAL_UNIS_PORT)}],
                          **{"preload": ["nodes", "services"]})
             return rt
         except:
             log.warn("Could not contact UNIS at {}, retrying...".format(url))
         time.sleep(5)
     
-def run(n, s):
+def run(sess, n, s, dldir):
     i=0
     while True:
         (i%5) or log.info("Waiting for some action...")
         if s.status == "UPDATE":
             dl_list = s.new_exnodes
-            log.debug("List of exnodes to download locally: {}".format(dl_list))
+            log.info("Caught UPDATE status with {} new exnodes".format(len(dl_list)))
+            for f in dl_list:
+                log.debug("Downloading: {} ({} bytes)".format(f.name, f.size))
+                try:
+                    diff, dsize, res = sess.download(f.selfRef, "{}/{}".format(dldir,f.name))
+                except Exception as e:
+                    log.error("Could not download file: {}".format(e))
+                    continue
+                if dsize != res.size:
+                    log.warn("WARNING: {}: transferred {} of {} bytes \
+(check depot file)".format(res.name,
+                           dsize,
+                           res.size))
+                else:
+                    print ("{0} ({1} {2:.2f} MB/s) {3}".format(res.name, res.size,
+                                                               res.size/1e6/diff,
+                                                               res.selfRef))
             time.sleep(1)
             s.status = "READY"
             s.commit()
@@ -98,6 +120,8 @@ def main():
                         help='UNIS instance for registration and metadata')
     parser.add_argument('-n', '--name', type=str, default=None,
                         help='Set ferry node name (ignore system hostname)')
+    parser.add_argument('-d', '--download', type=str, default=DOWNLOAD_DIR,
+                        help='Set local download directory')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Produce verbose output from the script')
     parser.add_argument('-q', '--quiet', action='store_true',
@@ -108,20 +132,35 @@ def main():
     log = init_logging(args)
     
     name = socket.gethostname()
+    fqdn = socket.getfqdn()
     log.info("Ferry \"{}\" reporting for duty".format(name))
     if args.name:
         name = args.name
         log.info("Setting ferry name to \"{}\"".format(name))
 
+    dldir = args.download
+    try:
+        os.makedirs(dldir)
+    except FileExistsError:
+        pass
+    except OSError as exp:
+        raise exp
+    
     # get our initial UNIS-RT instance
     rt = init_runtime(args.host)
-    
+
+    LOCAL_DEPOT={"ibp://{}:6714".format(fqdn): { "enabled": True}}
+    #LOCAL_DEPOT={"ibp://wdln-ferry-00:6714": { "enabled": True}}
+    sess = libdlt.Session([{"default": True,
+                            "url": "http://{}:{}".format(LOCAL_UNIS_HOST,LOCAL_UNIS_PORT)}],
+                          bs="5m", depots=LOCAL_DEPOT, threads=1)
+
     # Start the registration loop
     # returns handles to the node and service objects
-    (n,s) = register(rt, name)
+    (n,s) = register(rt, name, fqdn)
 
     # run our main loop
-    run(n,s)
+    run(sess, n, s, dldir)
     
 if __name__ == "__main__":
     main()
