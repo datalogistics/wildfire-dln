17fafba48db67e94453364f2fb5073968cdc28ba
Ezra Kissel <ezkissel@indiana.edu>
2018-01-02 16:55:05 -0500

    Enable a local-only ferry mode. The local mode subscribes for exnode objects and downloads them as soon as they are posted. TODO: resubscribe to local UNIS on reconnect.

--- ferry/dln_ferry.py
+++ ferry/dln_ferry.py
@@ -17,9 +17,11 @@ FERRY_SERVICE="http://unis.crest.iu.edu/schema/ext/dln/1/ferry#"
 UNIS_URL="http://localhost:8888"
 LOCAL_UNIS_HOST="localhost"
 LOCAL_UNIS_PORT=9000
-DOWNLOAD_DIR="/depot/web"
 
+# globals
+DOWNLOAD_DIR="/depot/web"
 log = None
+sess = None
 
 DLNFerry = schemaLoader.get_class(FERRY_SERVICE)
 GeoLoc = schemaLoader.get_class(GEOLOC)
@@ -66,43 +68,64 @@ def register(rt, name, fqdn):
 
     return (n,s)
     
-def init_runtime(remote, local):
+def init_runtime(remote, local, local_only):
     while True:
         try:
-            rt = Runtime([{"default": True, "url": remote}, {"url": local}],
-                         **{"preload": ["nodes", "services"]})
+            if local_only:
+                urls = [{"default": True, "url": local}]
+                opts = {"preload": ["nodes", "services", "exnodes"], "subscribe": {"exnodes": file_cb}}
+                log.debug("Connecting to UNIS instance(s): {}".format(local))
+            else:
+                urls = [{"default": True, "url": remote}, {"url": local}]
+                opts = {"preload": ["nodes", "services"]}
+                log.debug("Connecting to UNIS instance(s): {}".format(remote+','+local))
+            rt = Runtime(urls, **opts)
             return rt
-        except:
-            log.warn("Could not contact UNIS servers {}, retrying...".format(remote+','+local))
+        except Exception as e:
+            #import traceback
+            #traceback.print_exc()
+            log.warn("Could not contact UNIS servers {}, retrying...".format(urls))
         time.sleep(5)
-    
-def run(sess, n, s, dldir, rt):
+
+def file_cb(ex):
+    local_download(sess, [ex])
+
+def local_download(sess, exnodes):
+    for f in exnodes:
+        log.info("Downloading: {} ({} bytes)".format(f.name, f.size))
+        try:
+            diff, dsize, res = sess.download(f.selfRef, "{}/{}".format(DOWNLOAD_DIR,f.name))
+        except Exception as e:
+            log.error("Could not download file: {}".format(e))
+            continue
+        if dsize != res.size:
+            log.warn("WARNING: {}: transferred {} of {} bytes \
+            (check depot file)".format(res.name,
+                                       dsize,
+                                       res.size))
+        else:
+            log.info("{0} ({1} {2:.2f} MB/s) {3}".format(res.name, res.size,
+                                                         res.size/1e6/diff,
+                                                         res.selfRef))
+            
+def run_local(sess, n, s, rt):
     i=0
     while True:
-        (i%5) or log.info("Waiting for some action...")
+        (i%5) or log.info("Waiting for some local action...")
+        i+=1
+        time.sleep(1)
+        
+def run_remote(sess, n, s, rt):
+    i=0
+    while True:
+        (i%5) or log.info("Waiting for some remote action...")
         if s.status == "UPDATE":
             dl_list = s.new_exnodes
             log.info("Caught UPDATE status with {} new exnodes".format(len(dl_list)))
-            for f in dl_list:
-                log.debug("Downloading: {} ({} bytes)".format(f.name, f.size))
-                try:
-                    diff, dsize, res = sess.download(f.selfRef, "{}/{}".format(dldir,f.name))
-                except Exception as e:
-                    log.error("Could not download file: {}".format(e))
-                    continue
-                if dsize != res.size:
-                    log.warn("WARNING: {}: transferred {} of {} bytes \
-(check depot file)".format(res.name,
-                           dsize,
-                           res.size))
-                else:
-                    log.debug("{0} ({1} {2:.2f} MB/s) {3}".format(res.name, res.size,
-                                                                  res.size/1e6/diff,
-                                                                  res.selfRef))
+            local_download(sess, dl_list)
             time.sleep(1)
             s.status = "READY"
             s.commit()
-            
         i+=1
         time.sleep(1)
     
@@ -114,6 +137,10 @@ def init_logging(args):
     return log
         
 def main():
+    global log
+    global DOWNLOAD_DIR
+    global sess
+    
     parser = argparse.ArgumentParser(description="DLN Mobile Ferry Agent")
     parser.add_argument('-H', '--host', type=str, default=UNIS_URL,
                         help='UNIS instance for registration and metadata')
@@ -121,12 +148,13 @@ def main():
                         help='Set ferry node name (ignore system hostname)')
     parser.add_argument('-d', '--download', type=str, default=DOWNLOAD_DIR,
                         help='Set local download directory')
+    parser.add_argument('-l', '--local', action='store_true',
+                        help='Run using only local UNIS instance (on-ferry)')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Produce verbose output from the script')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='Quiet mode, no logging output')
 
-    global log
     args = parser.parse_args()
     log = init_logging(args)
     
@@ -137,9 +165,9 @@ def main():
         name = args.name
         log.info("Setting ferry name to \"{}\"".format(name))
 
-    dldir = args.download
+    DOWNLOAD_DIR = args.download
     try:
-        os.makedirs(dldir)
+        os.makedirs(DOWNLOAD_DIR)
     except FileExistsError:
         pass
     except OSError as exp:
@@ -147,10 +175,15 @@ def main():
 
     # use fqdn to determine local endpoints
     LOCAL_DEPOT={"ibp://{}:6714".format(fqdn): { "enabled": True}}
-    LOCAL_UNIS = "http://{}:{}".format(fqdn, LOCAL_UNIS_PORT)
+
+    # allow an alternative UNIS instance (non-ferry) in local mode
+    if (args.local and args.host != UNIS_URL):
+        LOCAL_UNIS=args.host
+    else:
+        LOCAL_UNIS = "http://{}:{}".format(fqdn, LOCAL_UNIS_PORT)
     
     # get our initial UNIS-RT and libdlt sessions
-    rt = init_runtime(args.host, LOCAL_UNIS)
+    rt = init_runtime(args.host, LOCAL_UNIS, args.local)
     sess = libdlt.Session([{"default": True, "url": LOCAL_UNIS}],
                           bs="5m", depots=LOCAL_DEPOT, threads=1)
 
@@ -159,7 +192,10 @@ def main():
     (n,s) = register(rt, name, fqdn)
 
     # run our main loop
-    run(sess, n, s, dldir, rt)
+    if args.local:
+        run_local(sess, n, s, rt)
+    else:
+        run_remote(sess, n, s, rt)
     
 if __name__ == "__main__":
     main()
