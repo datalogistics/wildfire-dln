89bc51fb83f95e55baa397e56a94d4757c462646
Jeremy Musser <jemusser@indiana.edu>
2018-01-05 16:42:16 -0500

    Modifications for idms demo

--- idms/idms/app.py
+++ idms/idms/app.py
@@ -4,6 +4,7 @@ import json
 
 from idms.handlers import PolicyHandler, AuthHandler, SSLCheck
 from idms.lib.service import IDMSService
+from idms.lib.middleware import FalconCORS
 from unis import Runtime
 
 # TMP Mock database object
@@ -16,11 +17,11 @@ class _Database(object):
                 "data_lifetime": 21600
             },
             "Ferry-Mule-17": {
-                "ferry-name": "wdln-ferry-17",
+                "ferry_name": "wdln-ferry-17",
                 "data_lifetime": 108000
             },
             "Ferry-Drone-03": {
-                "ferry-name": "wdln-ferry-03",
+                "ferry_name": "wdln-ferry-03",
                 "data-lifetime": 3600
             }
         }
@@ -52,17 +53,17 @@ class _Database(object):
             self._store[filename] = []
         self._store[filename].append(policy)
 
-def _get_app(unis):
+def _get_app(unis, depots):
     conf = { "auth": False, "secret": "a4534asdfsberwregoifgjh948u12" }
     db = _Database()
-    rt = Runtime(unis, defer_update=True)
-    rt.addService(IDMSService)
+    rt = Runtime(unis, defer_update=True, preload=["nodes", "services"])
+    rt.addService(IDMSService(depots))
     auth      = AuthHandler(conf, db)
     policy    = PolicyHandler(conf, db, rt)
     
     ensure_ssl = SSLCheck(conf)
-    #app = falcon.API(middleware=[ensure_ssl])
-    app = falcon.API()
+    app = falcon.API(middleware=[FalconCORS()])
+    #app = falcon.API()
     app.add_route('/', policy)
     
     return app
@@ -70,23 +71,32 @@ def _get_app(unis):
 def main():
     from lace import logging
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-u', '--unis', default='http://localhost:8888', type=str,
+    parser.add_argument('-u', '--unis', default='http://wdln-base-station:8888', type=str,
                         help='Set the comma diliminated urls to the unis instances of interest')
+    parser.add_argument('-H', '--host', default='wdln-base-station', type=str, help='Set the host for the server')
     parser.add_argument('-p', '--port', default=8000, type=int, help='Set the port for the server')
     parser.add_argument('-d', '--debug', default=0, type=int, help='Set the log level')
+    parser.add_argument('-D', '--depots', default='', type=str, help='Provide a file for the depot decriptions')
     args = parser.parse_args()
     
-    logging.trace.setLevel([logging.NOTSET, logging.INFO, logging.DEBUG][args.debug])
+    from unis import logging as ulog
+    level = [logging.NOTSET, logging.INFO, logging.DEBUG][args.debug]
+    ulog.setLevel(level)
+    logging.getLogger().setLevel(level)
+    logging.trace.setLevel(level)
     port = args.port
     unis = [str(u) for u in args.unis.split(',')]
-    print(unis)
-    app = _get_app(unis)
+    depots = None
+    if args.depots:
+        with open(args.depots) as f:
+            depots = json.load(f)
+    app = _get_app(unis, depots)
     
     from wsgiref.simple_server import make_server
-    server = make_server('localhost', port, app)
+    server = make_server(args.host, port, app)
     port = "" if port == 80 else port
     print("Getting topology from {}".format(unis))
-    print("Listening on {}{}{}".format('http://localhost',":" if port else "", port))
+    print("Listening on {}{}{}".format(args.host,":" if port else "", port))
     server.serve_forever()
     
 if __name__ == "__main__":
--- idms/idms/lib/service.py
+++ idms/idms/lib/service.py
@@ -1,40 +1,84 @@
+import itertools
+
+from unis import Runtime
 from unis.models import Service, Exnode, Extent
 from unis.services import RuntimeService
 from collections import defaultdict
 from libdlt import Session
-from libdlt.schedule import AbstractSchedule, BaseDownloadSchedule
+from libdlt.schedule import BaseUploadSchedule, BaseDownloadSchedule
+from lace import logging
+
+
+
+from pprint import pprint
 
 class IDMSService(RuntimeService):
-    class ForceUpload(AbstractSchedule):
+    class ForceUpload(BaseUploadSchedule):
         def __init__(self, sources):
-            self._ls = cycle(sources)
+            self._ls = itertools.cycle(sources)
         def setSource(self, source):
             pass
+    class ForceDownload(BaseDownloadSchedule):
+        def __init__(self, target):
+            self.target = target
+        def setSource(self, source):
+            self._ls = defaultdict(list)
+            for ext in filter(lambda x: x.location != self.target, source):
+                self._ls[ext.offset].append(ext)
     
     targets = [Service]
+    lock = False
     
+    def __init__(self, depots):
+        super(IDMSService, self).__init__()
+        self._depots = depots
+        self._log = logging.getLogger()
+        
     def attach(self, runtime):
         super(IDMSService, self).attach(runtime)
-        self._session = Session(runtime._unis._default_source, None)
+        self._session = Session(runtime._unis._default_source, self._depots, viz_url="http://wdln-base-station:42424")
         runtime.pending_exnodes = defaultdict(list)
     
     def update(self, resource):
-        if resource.STATUS == 'READY' and self._runtime.pending_exnodes[resource.name]:
+        print("Resource touched")
+        self._log.info("Service touched - {}".format(resource.name))
+        while self.lock:
+            pass
+        if resource.status == 'READY' and self._runtime.pending_exnodes[resource.name]:
             try:
-                tmpRT = Runtime(resource.unis_url, defer_update=True)
-            except Exception as exp:
-                self._runtime.log.warn("Failed to contact transient UNIS - {}".format(exp))
-                return
-                
-            upload = ForceUpload(resource.accessPoint)
-            for exnode, lifetime in self._runtime.pending_exnodes[resource.name]:
-                self._session.copy(exnode.selfRef, BaseDownloadSchedule(), upload, duration=lifetime)
-                new_exnode = Exnode({k:v for k,v in exnode.to_JSON() if k not in ["id", "selfRef", "extents"]})
-                new_exnode.extents = [x for x in exnode.extents if x.location == resource.accessPoint]
-                tmpRT.insert(new_exnode, commit=True)
-                resource.new_exnodes.append(new_exnode)
-                
-            tmpRT.flush()
-            self._runtime.pending_exnodes[resource.name].clear()
-            resource.STATUS = 'UPDATE'
-            self._runtime.flush()
+                type(self).lock = True
+                try:
+                    tmpRT = Runtime(resource.unis_url, subscribe=False, defer_update=True)
+                except Exception as exp:
+                    self._runtime.log.warn("Failed to contact transient UNIS - {}".format(exp))
+                    return
+            
+                todo = self._runtime.pending_exnodes[resource.name]
+                self._runtime.pending_exnodes[resource.name] = []
+                upload = self.ForceUpload(resource.accessPoint if isinstance(resource.accessPoint, list) else [resource.accessPoint])
+                for exnode, lifetime in todo:
+                    print("Starting copy")
+                    try:
+                        self._session.copy(exnode.selfRef, self.ForceDownload(resource.accessPoint), upload, duration=lifetime)
+                    except Exception as exp:
+                        import sys
+                        print(sys.exc_info()[2])
+                    print("Copy complete")
+                    new_exnode = Exnode({k:v for k,v in exnode.to_JSON().items() if k not in ['id', 'selfRef', 'extents']})
+                    for extent in exnode.extents:
+                        if extent.location == resource.accessPoint:
+                            new_extent = Extent({k:v for k,v in extent.to_JSON().items() if k not in ["id", "selfRef", "parent"]})
+                            new_extent.parent = new_exnode
+                            tmpRT.insert(new_extent, commit=True)
+                            new_exnode.extents.append(new_extent)
+                            pprint(new_extent.to_JSON())
+                    tmpRT.insert(new_exnode, commit=True)
+                    resource.new_exnodes.append(new_exnode)
+                tmpRT.flush()
+                resource.status = 'UPDATE'
+                self._runtime.flush() 
+            except:
+                raise
+            finally:
+                type(self).lock = False
+   
