deff131caaea1c1513d1016939ed4bd44edb558d
Ezra Kissel <ezkissel@indiana.edu>
2018-05-15 16:09:44 -0400

    add some output suitable for pasting into local /etc/hosts

--- docker/wdln_run.sh
+++ docker/wdln_run.sh
@@ -4,3 +4,8 @@ docker run --name wdln-base --network wdln --hostname wdln-base -it -d wdln-base
 docker run --name wdln-ferry-00 --network wdln --hostname wdln-ferry-00 -it -d wdln-ferry
 docker run --name wdln-ferry-01 --network wdln --hostname wdln-ferry-01 -it -d wdln-ferry
 docker run --name wdln-idms --network wdln --hostname wdln-idms -it -d wdln-idms
+
+echo
+echo
+
+docker network inspect --format='{{range .Containers}}{{.IPv4Address}} {{println .Name}}{{end}}' wdln | sed 's/\/16//g'
