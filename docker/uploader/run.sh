7a0213505b2e25553e679ab1510c738e65642f0c
Ezra Kissel <ezkissel@indiana.edu>
2018-05-17 12:58:06 -0400

    enable download viz

--- docker/uploader/run.sh
+++ docker/uploader/run.sh
@@ -5,4 +5,4 @@ HOSTNAME=`hostname`
 git -C unisrt pull
 git -C libdlt pull
 
-dlt_xfer -d ~/.depots -H http://wdln-base:9000 -u -r /wdln
+dlt_xfer -d ~/.depots -H http://wdln-base:9000 -V http://wdln-base:42424 -u -r /wdln
