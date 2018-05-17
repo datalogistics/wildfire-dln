9dfd384e9e9d75bb2bdf96608c2c25f5afd83873
Ezra Kissel <ezkissel@indiana.edu>
2018-05-17 15:08:30 -0400

    extend uploader to allow upload to specific nodes but libdlt still ignores "enabled" attribute in depot list

--- docker/uploader/run.sh
+++ docker/uploader/run.sh
@@ -5,4 +5,8 @@ HOSTNAME=`hostname`
 git -C unisrt pull
 git -C libdlt pull
 
-dlt_xfer -d ~/.depots -H http://wdln-base:9000 -V http://wdln-base:42424 -u -r /wdln
+echo "Uploading to ${TARGET}..."
+
+sed -i "/${TARGET}/{n;s/\"enabled\": false/\"enabled\": true/}" .depots
+
+dlt_xfer -d ~/.depots -H http://${TARGET}:9000 -V http://wdln-base:42424 -u -r /wdln
--- docker/wdln_uploader.sh
+++ docker/wdln_uploader.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+    echo "Usage: $0 [wdln-base|wdln-ferry-00|...]"
+    exit 1
+fi
+
+TARGET=$1
 UPLOAD_DIR=/tmp/wdln
 
-docker run --net wdln -v ${UPLOAD_DIR}:/wdln:ro -it wdln-uploader
+docker run --net wdln -v ${UPLOAD_DIR}:/wdln:ro -it -e TARGET=${TARGET} wdln-uploader
