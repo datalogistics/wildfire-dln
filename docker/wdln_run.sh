63eb7fa3768dc25a72594e3ebf3f1f58d6ad4e0a
Jeremy Musser <jemusser@indiana.edu>
2018-05-18 14:00:35 -0400

    Removed debugging commit

--- docker/wdln_run.sh
+++ docker/wdln_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --name wdln-base --network wdln --hostname wdln-base -it -d wdln-base
-docker run --name wdln-ferry-00 --network wdln --hostname wdln-ferry-00 -it -d -v /home/jemusser/open/wildfire-dln:/home/wdln/wildfire-dln wdln-ferry 
+docker run --name wdln-ferry-00 --network wdln --hostname wdln-ferry-00 -it -d 
 docker run --name wdln-ferry-01 --network wdln --hostname wdln-ferry-01 -it -d wdln-ferry
 #docker run --name wdln-idms --network wdln --hostname wdln-idms -it -d wdln-idms
 
