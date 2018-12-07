c7ced0b6b68cddbc61463ae6d65b78d10362a8c8
jzerick <31254143+jzerick@users.noreply.github.com>
2018-12-06 21:46:10 -0500

    Update whisper.cpp

--- lora/whisper/whisper.cpp
+++ lora/whisper/whisper.cpp
@@ -1,28 +1,24 @@
-/*******************************************************************************
+/******************************************************************************
+
 File: whisper.cpp
-Language: C++
-
-This code provides an interface between the low-level methods in whisper_lora.h
-and whisper.py. 
-
-This layer serves as a gatekeeper, filtering expected abundant "noise" in the field
-from messages of interest. Protocol and switch behaviors are handled in 
-whisper.py, but at some point could be encoded in the C++ layer for the sake of
-speed. This code could also be combined with whisper_lora.h if a suitable 
-replacement for the Standard Template Library is found. For now, whisper.cpp
-and whisper.py communicate via sockets.
-
-A number of threads were used to provide resiliency. While the threads do
-logically separate production and consumption of messages for ease of 
-troubleshooting, assuming the device this code runs on has multiple cores 
-(the RPi 3B+ has four), the device can nearly always remain in receive mode,
-further avoiding loss of messages. The use of queues adds a cushion as well.
-
 Author: Juliette Zerick (jzerick@iu.edu)
-OPEN Lab, Indiana University
+	for the WildfireDLN Project
+	OPEN Networks Lab at Indiana University-Bloomington
+
+whisper.cpp provides the interface between C drivers (whisper_lora.h) and
+higher-level switch behavior coded in Python (whisper.py). Lower-level
+functions for interacting with the LoRa transceiver in the LoRa Hat
+manufactured by Dragino and passes the data on via sockets to Python
+counterparts for processing. 
+
+Last modified: December 6, 2018 
+
 *******************************************************************************/
 
-#include "whisper_lora.h" 
+
+
+
+#include "whisper_lora.h"
 
 #include <netdb.h>
 #include <stdbool.h> // for bool type
@@ -30,15 +26,14 @@ OPEN Lab, Indiana University
 #include <queue> 
 #include <net/if.h>
 
-#define PORT_OF_INTEREST    5683    
 #define SNOOZE_TIME         5000 // in milliseconds
 
-#define JUST_BROADCASTING   false
+#define JUST_BROADCASTING   false // for debugging
 
 #define INCOMING_ADDR       "127.0.0.1"
 #define OUTGOING_ADDR       "127.0.0.1"
 
-#define USING_MINION_PROTOCOL true // need to find a better name 
+#define USING_MONITOR       false
 
 //*******************************************************************************
 
@@ -50,7 +45,6 @@ pthread_mutex_t lora_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t inbox_q_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t outbox_q_lock = PTHREAD_MUTEX_INITIALIZER;
 
-//Flag for Ctrl-C - from RH examples
 volatile sig_atomic_t closing_time = false;
 
 queue<char*> inbox_q;
@@ -58,14 +52,18 @@ queue<char*> outbox_q;
 
 void mopup();
 
-char hello[] = "coap://localhost/time";
+// for broadcasting, optional predefined packets to send out
+char notif_pkt[] = "hello";
 
 char* MY_MAC_ADDR = new char[13];
 
 //*******************************************************************************
 
-// TODO properly cite
-// https://stackoverflow.com/questions/6767296/how-to-get-local-ip-and-mac-address-c
+// Solution from Friek (2011) at StackOverflow
+// in response to the following posted question:
+// "How to get local IP and MAC address C [duplicate]" available at
+// <https://stackoverflow.com/questions/6767296/how-to-get-local-ip-and-mac-address-c>
+// last accessed: November 27, 2018
 void get_mac_eth0(char* MAC_str){
     #define HWADDR_len 6
     int s,i;
@@ -81,78 +79,73 @@ void get_mac_eth0(char* MAC_str){
     MAC_str[12]='\0';
 }
 
+// Initially the RadioHead libraries were used but were found to be unstable. This
+// bit of code survived.
+// Author: hallard at GitHub
+// Library: RadioHead
+// File: RadioHead/examples/raspi/rf95/rf95_client.cpp
+// Repo online: <https://github.com/hallard/RadioHead/blob/master/examples/raspi/rf95/rf95_client.cpp>
+// Last updated August 20, 2018
+// Last accessed: November 27, 2018
 void sig_handler(int sig){
     closing_time = true;
     delay(SNOOZE_TIME);
     mopup();
-} 
-
-// used for testing
-void broadcast(){
-    while(!closing_time){
-        transmit_via_lora(hello,strlen(hello));
-        delay(SNOOZE_TIME); // in milliseconds
-    }
 }
 
-// thread-safe removal of a message from the queue
 char* lock_pop(queue<char*> &q,pthread_mutex_t &lock){
     char* msg = NULL;
 
     pthread_mutex_lock(&lock);
-
     if(q.size() > 0){
         msg = q.front();
         q.pop();
     }
-
     pthread_mutex_unlock(&lock);
-
+    
     return msg;
 }
 
-// thread-safe push of a message to the queue
 void lock_push(queue<char*> &q,char* msg,pthread_mutex_t &lock){
     pthread_mutex_lock(&lock);
     q.push(msg);
     pthread_mutex_unlock(&lock);
 }
 
-// slice a set of messages from the socket, then push
 void split_lock_push(queue<char*> &q,char* msgs,pthread_mutex_t& lock){
-    printf("%s\n",msgs);
+    printf("split_lock_push: received %s\n",msgs);
 
-    if (! USING_MINION_PROTOCOL){
-        lock_push(q,msgs,lock);
-        return;
-    }
-
-    // TODO retest 
     int total_msg_len = strlen(msgs);
     int idx = 0;
+    int msg_start = 0;
     
     while (idx < total_msg_len){
-        if (msgs[idx] == '|'){ // at end of a message
-            msgs[idx] = '\0'; // plant a termination character 
-
-            lock_push(q,msgs,lock);
-            printf("\tpushed %s\n",msgs);
-
-            idx++;
-
-            if(idx < total_msg_len)
-                msgs = &msgs[idx];
-	} else {
-            idx++; // should be on the first character of the To address of a new message
-	}
+        if (msgs[idx] == '|' or msgs[idx] == ':'){ // at end of a message
+            msgs[idx] = '\0'; // plant a termination character now
+            lock_push(q,&(msgs[msg_start]),lock);
+	    msg_start = idx + 1;
+	}  
+ 	idx++; // should be on the first character of the To address of a new message
     }
 }
 
-// enacts the black hole protocol
-bool not_noise(char* msg){
-    if(!USING_MINION_PROTOCOL) 
-        return true;    
+bool has_saturation_req(char*msg){
+    unsigned int i;
+    
+    for(i=0; i<strlen(msg); i++){
+        if(msg[i] == '/'){
+            if(i + 1 < strlen(msg) && msg[i+1] == '0'){
+                return false;}
+            else{
+                return true;}
+	}
+    }
+                
+    return false;
+}
 
+bool not_noise(char* msg){
+/**
     int total_msg_len = strlen(msg);
 
     // is it a multicast message?
@@ -161,63 +154,78 @@ bool not_noise(char* msg){
 
     // is it a message meant for this device?
     int i;
-    for (i=0;i<12;i++)
-        if(msg[i] != MY_MAC_ADDR[i])
-            return false;
-
+    for (i=0;i<12;i++){
+        if(msg[i] != MY_MAC_ADDR[i]){
+            if(has_saturation_req(msg)){
+                return true;}
+            else{
+                return false;}
+	}
+    }
+**/
     return true;
 }
 
-// listens on LoRa and adds received messages to the inbox_q. it is assumed that
-// the bulk of messages received will not be relevant. not_noise() quickly sorts
-// out the noise.
+// listens on LoRa and pushes received messages to the inbox queue
 void* lora_listener(void* arg){
     char* msg = new char[MESSAGE_LEN];
     int* msg_len = new int;
+    int* rssi_val = new int;
+    bool got_something;
+    long int switch_time = 0; // if ever needed
 
-    // radio init
-    put_in_neutral();
-    opmode(OPMODE_RX);
-
+    printf("lora_listener: active\n");
+    
     while(!closing_time){ 
         pthread_mutex_lock(&lora_lock);
+	switch_time = switch_to_receive();
+        got_something = listen_for_lora(msg,msg_len,rssi_val);
+	pthread_mutex_unlock(&lora_lock);
 
-        // TODO check to make sure this doesn't introduce a bottleneck
-        if(listen_for_lora(msg,msg_len)){
-            pthread_mutex_unlock(&lora_lock);
-
-            if(not_noise(msg)){
-                lock_push(inbox_q,msg,inbox_q_lock);
-                printf("inbox tender: msg pushed\n");
+	if(got_something){
+            // overwrite the : and note that the buffer is already filled with 0
+            sprintf(&(msg[*msg_len-1]),"%d|",*rssi_val);
+ 
+  	    if(not_noise(msg)){
+                split_lock_push(inbox_q,msg,inbox_q_lock);
+                printf("lora_listener: message placed in inbox, %s\n",msg);
+		//delete msg; // left as a reminder: don't do this
                 msg = new char[MESSAGE_LEN];
             }else{
                 memset(msg,0,MESSAGE_LEN);
             }
+	}
 
-        }else{
-            pthread_mutex_unlock(&lora_lock);
-        }
-
-        delay(1); //  in milliseconds
+	delay(1000); //  note: in milliseconds!
     }
     
-    close(sock_raw);
     return (void*) NULL;
 }
 
-// takes messages from outbox_q and sends them via LoRa when possible
+// TODO add a socket to listen on for datagrams, will not perform error checking
+// takes messages from the outbox queue and transmits them via LoRa when possible
 void* lora_speaker(void* arg){
     char* msg;
+    long int switch_time = 0; // if ever needed
+    bool success = false;
+
+    printf("lora_speaker: active\n");
 
     while(!closing_time){
         msg = lock_pop(outbox_q,outbox_q_lock);
         
         if(msg != NULL){
-            printf("outbox_tender: pulled this out of the queue%s\n",msg);
+            printf("lora_speaker: received from outbox queue %s\n",msg);
 
             pthread_mutex_lock(&lora_lock);
-            transmit_via_lora(msg,strlen(msg));
+	    switch_time = switch_to_transmit();
+            success = transmit_via_lora(msg,strlen(msg));
             pthread_mutex_unlock(&lora_lock);
+
+	    if(!success){
+                printf("lora_speaker: TRANSMIT FAILURE\n"); 
+		lock_push(outbox_q,msg,outbox_q_lock);
+	    }
         }
 
         delay(SNOOZE_TIME); // in milliseconds
@@ -226,10 +234,13 @@ void* lora_speaker(void* arg){
     return (void*) NULL;    
 }
 
-// listens for messages from whisper.py via socket, then adds them to the outbox_q 
+// py_speaker and py_listener were based on code from Silver Moon at BinaryTides
+// "Receive full data with recv socket function in C"
+// published online September 8, 2012
+// and available online at 
+// <https://www.binarytides.com/receive-full-data-with-recv-socket-function-in-c/>
+// last accessed: November 27, 2018
 void* py_listener(void* arg){
-    // TODO properly cite
-    // frrom https://www.binarytides.com/receive-full-data-with-recv-socket-function-in-c/
     int socket_desc;
     struct sockaddr_in server;
 
@@ -239,50 +250,55 @@ void* py_listener(void* arg){
     //Create socket
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     while (socket_desc == -1){
-        //printf("Cinc: trying to establish socket\n");
+        printf("py_listener: attempting to establish socket\n");
         socket_desc = socket(AF_INET , SOCK_STREAM , 0);
         delay(SNOOZE_TIME);
     }
 
-    //printf("Cinc: created socket\n");
-
+    //ip address of www.msn.com (get by doing a ping www.msn.com at terminal)
     server.sin_addr.s_addr = inet_addr(INCOMING_ADDR);
     server.sin_family = AF_INET;
     server.sin_port = htons( INCOMING_PORT );
  
     //Connect to remote server
     while (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
-        //printf("Cinc: connect error\n");
+        printf("py_listener: attempting to connect to Python counterpart\n");
         delay(SNOOZE_TIME);
     }
      
-    //printf("Cinc: Connected!\n");
+    printf("py_listener: active and connected\n");
 
     while(!closing_time){ 
         memset(msg,0,MESSAGE_LEN);
 
         if(recv(socket_desc, msg , MESSAGE_LEN , 0) < 0 || strlen(msg) == 0){
-            //printf("Cinc: recv failed");
+            printf("py_listener: recv failed");
             delay(SNOOZE_TIME);
             continue;
         }
 
-        printf("Cinc: data received %s \n",msg);
+        printf("py_listener: data received %s \n",msg);
 
         // insert into outbox queue here
-        split_lock_push(outbox_q,msg,outbox_q_lock);
+        lock_push(outbox_q,msg,outbox_q_lock);
 
         msg = new char[MESSAGE_LEN];
         delay(SNOOZE_TIME);
     }
     
-    //close(sock_raw);
+    shutdown(socket_desc,2);
     return (void*) NULL;
 }
 
-// pulls messages from the inbox_q and passes them to whisper.py 
+// pulls messages from the inbox queue, then sends them out via socket
+
+// py_speaker and py_listener were based on code from Silver Moon at BinaryTides
+// "Receive full data with recv socket function in C"
+// published online September 8, 2012
+// and available online at 
+// <https://www.binarytides.com/receive-full-data-with-recv-socket-function-in-c/>
+// last accessed: November 27, 2018
 void* py_speaker(void* arg){
-    // frrom https://www.binarytides.com/receive-full-data-with-recv-socket-function-in-c/
     int socket_desc;
     struct sockaddr_in server;
     int OUTGOING_PORT = *(int*)arg;
@@ -294,47 +310,64 @@ void* py_speaker(void* arg){
 
     while (socket_desc == -1){
         socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-        //printf("Cout: trying to establish socket\n");
+        printf("py_speaker: attempting to establish socket\n");
         delay(SNOOZE_TIME);
     }
 
-    //printf("Cout: created socket\n");
-
+    //ip address of www.msn.com (get by doing a ping www.msn.com at terminal)
     server.sin_addr.s_addr = inet_addr(OUTGOING_ADDR);
     server.sin_family = AF_INET;
     server.sin_port = htons(OUTGOING_PORT);
  
     //Connect to remote server
     while (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
-        //printf("Cout: connect error\n");
+        printf("py_speaker: attempting to connect to Python counterpart\n");
         delay(SNOOZE_TIME);
     }
 
-    //printf("Cout: Connected\n");
+    printf("py_speaker: active and connected\n");
 
     while(!closing_time){
         // try to pull from inbox qeuue
         if(inbox_q.size() > 0){
             msg = lock_pop(inbox_q,inbox_q_lock);
         
-            printf("Cout: pulled this off the queue %s\n",msg);
+            printf("py_speaker: retrieved from the inbox queue %s\n",msg);
 
             if(send(socket_desc , msg , strlen(msg) , 0) < 0){
-                //printf("Cout: send failed");
+                printf("py_speaker: send failed");
                 delay(SNOOZE_TIME);
+		
+		lock_push(inbox_q,msg,inbox_q_lock);
                 continue;
             }
 
-        //printf("Cout: data sent %s of length %d\n",outgoing_message,strlen(outgoing_message));
-        //printf("Cout: data sent %s of length %d\n",msg,strlen(msg));
+        printf("py_speaker: sent message %s of length %d\n",msg,strlen(msg));
         delay(SNOOZE_TIME);
         }        
     }
-
+    shutdown(socket_desc,2);
     return (void*) NULL;
 }
 
-// cleaning up prior to shutdown
+void broadcast(){
+    long int switch_time = 0; // if ever needed
+    bool success = false;
+
+    while(!closing_time){
+        pthread_mutex_lock(&lora_lock);
+	switch_time = switch_to_transmit();
+        success = transmit_via_lora(notif_pkt,strlen(notif_pkt));
+        pthread_mutex_unlock(&lora_lock);
+
+        if(!success){
+            printf("could not broadcast!\n");
+	}
+
+	delay(SNOOZE_TIME); // in milliseconds
+    }
+}
+
 void mopup(){ 
     //pthread_join(port_listener_t, NULL); // insufficient
 
@@ -346,28 +379,29 @@ void mopup(){
     }
         
     writeReg(RegPaRamp, 0x00);
-    put_in_neutral();
+    opmode(OPMODE_SLEEP);
     exit(0);
 }
 
 int main (int argc, char *argv[]) {
-    if(argc != 3){
+    int py_inc_port = -1;
+    int py_out_port = -1;
+
+    if(argc != 3 && !JUST_BROADCASTING){
         printf("usage: sudo ./whisper <incoming port> <outgoing port>\n");
         exit(1);
+    }
+    
+    if(!JUST_BROADCASTING){
+        py_inc_port = atoi(argv[1]);
+        py_out_port = atoi(argv[2]);
     }
 
     get_mac_eth0(MY_MAC_ADDR);
 
-    int py_inc_port = atoi(argv[1]);
-    int py_out_port = atoi(argv[2]);
-
-    // it's 0
-    //printf("default value? %x\n",readReg(RegPaRamp));
-
-    // for a coordinated, clean shutdown
+    // for graceful shutdowns
     signal(SIGINT, sig_handler);
 
-    // set up the RPi/Hat
     wiringPiSetup () ;
     pinMode(ssPin, OUTPUT);
     pinMode(dio0, INPUT);
@@ -376,24 +410,34 @@ int main (int argc, char *argv[]) {
     wiringPiSPISetup(CHANNEL, 500000);
 
     SetupLoRa();
+    put_in_neutral();
+    opmode(OPMODE_RX);
+    listening = true;
 
-    if (JUST_BROADCASTING){
-        broadcast(); // endless loop until killed
-    } // if broadcasting, none of the code below will execute
-
-    // summon the army of threads
-    pthread_create(&py_listener_t, NULL, &py_listener, (void*) &py_inc_port);
-    pthread_create(&lora_listener_t, NULL, &lora_listener, (void*) NULL);
-    pthread_create(&py_speaker_t, NULL, &py_speaker, (void*) &py_out_port);
-    pthread_create(&lora_speaker_t, NULL, &lora_speaker, (void*) NULL);
-    
-    // wait until it's time to shut down
-    while(!closing_time){
-        delay(SNOOZE_TIME);
+    // defined in whisper_lora.h
+    if(USING_MONITOR){
+        pthread_create(&monitor_t, NULL, &monitor, (void*) NULL);
     }
 
-    mopup();
+    pthread_create(&lora_listener_t, NULL, &lora_listener, (void*) NULL);
+    pthread_create(&lora_speaker_t, NULL, &lora_speaker, (void*) NULL);
 
-    return (0);
+    if (JUST_BROADCASTING){
+        broadcast(); // endless loop until manually killed
+    } // hence if broadcasting, none of the code below will execute
+
+
+    pthread_create(&py_listener_t, NULL, &py_listener, (void*) &py_inc_port);
+    pthread_create(&py_speaker_t, NULL, &py_speaker, (void*) &py_out_port);
+
+    //while(!closing_time){
+    //    delay(SNOOZE_TIME);
+    //}
+
+    delay(SNOOZE_TIME*3);
+
+    printf("shutting down\n");
+    mopup();
+    return 0;
 }
 
